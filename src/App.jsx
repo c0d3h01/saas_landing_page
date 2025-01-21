7f953ee7fe8a4200a8d66b4e911a2fc4b16d9e8d
Harshal Sawant <c0d3h01@gmail.com>
2025-01-21 22:51:33 +0530

    incomplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./sections/Header.jsx";
+import Hero from "./sections/Hero.jsx";
+import Features from "./sections/Features.jsx";
 
 const App = () => {
-    return (
+  return (
     <main className="overflow-hidden">
-        <Header />
+      <Header />
+      <Hero />
+      <Features />
     </main>
-    )
-}
-export default App
+  );
+};
+export default App;
---
+++ src/constants/index.jsx
@@ -0,0 +1,24 @@
+export const features = [
+  {
+    id: "0",
+    icon: "/images/features-1.png",
+    caption: "Feature 1",
+    title: "Feature 1",
+    text: "TEXT 1",
+    button: {
+      icon: "/images/magictouch.svg",
+      title: "Watch the demo",
+    },
+  },
+  {
+    id: "1",
+    icon: "/images/features-2.png",
+    caption: "Feature 2",
+    title: "Feature 2",
+    text: "TEXT 2",
+    button: {
+      icon: "/images/magictouch.svg",
+      title: "Watch the demo",
+    },
+  },
+];
